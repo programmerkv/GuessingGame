3b56df3f455953bd3c91421ea87997617c425ef0
Khoa Vo <programmer.kv@gmail.com>
2018-03-03 20:15:52 +0700

    Original commit

---
+++ GuessingGame/Form.Designer.cs
@@ -0,0 +1,113 @@
+﻿namespace GuessingGame
+{
+    partial class Form
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.tb = new System.Windows.Forms.TextBox();
+            this.btnGuess = new System.Windows.Forms.Button();
+            this.btnReset = new System.Windows.Forms.Button();
+            this.lbMessage = new System.Windows.Forms.Label();
+            this.lbHighscore = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+            // 
+            // tb
+            // 
+            this.tb.Location = new System.Drawing.Point(16, 20);
+            this.tb.Name = "tb";
+            this.tb.Size = new System.Drawing.Size(190, 26);
+            this.tb.TabIndex = 0;
+            // 
+            // btnGuess
+            // 
+            this.btnGuess.Location = new System.Drawing.Point(218, 18);
+            this.btnGuess.Name = "btnGuess";
+            this.btnGuess.Size = new System.Drawing.Size(80, 36);
+            this.btnGuess.TabIndex = 1;
+            this.btnGuess.Text = "Guess";
+            this.btnGuess.UseVisualStyleBackColor = true;
+            this.btnGuess.Click += new System.EventHandler(this.btnGuess_Click);
+            // 
+            // btnReset
+            // 
+            this.btnReset.Location = new System.Drawing.Point(304, 18);
+            this.btnReset.Name = "btnReset";
+            this.btnReset.Size = new System.Drawing.Size(80, 36);
+            this.btnReset.TabIndex = 2;
+            this.btnReset.Text = "Reset";
+            this.btnReset.UseVisualStyleBackColor = true;
+            this.btnReset.Click += new System.EventHandler(this.btnReset_Click);
+            // 
+            // lbMessage
+            // 
+            this.lbMessage.AutoSize = true;
+            this.lbMessage.Location = new System.Drawing.Point(9, 59);
+            this.lbMessage.Name = "lbMessage";
+            this.lbMessage.Size = new System.Drawing.Size(192, 20);
+            this.lbMessage.TabIndex = 3;
+            this.lbMessage.Text = "> Please enter your guess";
+            // 
+            // lbHighscore
+            // 
+            this.lbHighscore.AutoSize = true;
+            this.lbHighscore.Location = new System.Drawing.Point(9, 91);
+            this.lbHighscore.Name = "lbHighscore";
+            this.lbHighscore.Size = new System.Drawing.Size(130, 20);
+            this.lbHighscore.TabIndex = 4;
+            this.lbHighscore.Text = "> Highscore - 0%";
+            // 
+            // Form
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(9F, 20F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.SystemColors.Control;
+            this.ClientSize = new System.Drawing.Size(398, 124);
+            this.Controls.Add(this.lbHighscore);
+            this.Controls.Add(this.lbMessage);
+            this.Controls.Add(this.btnReset);
+            this.Controls.Add(this.btnGuess);
+            this.Controls.Add(this.tb);
+            this.ForeColor = System.Drawing.Color.Black;
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedSingle;
+            this.MaximizeBox = false;
+            this.Name = "Form";
+            this.Text = "Guessing Game";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.TextBox tb;
+        private System.Windows.Forms.Button btnGuess;
+        private System.Windows.Forms.Button btnReset;
+        private System.Windows.Forms.Label lbMessage;
+        private System.Windows.Forms.Label lbHighscore;
+    }
+}
+
---
+++ GuessingGame/Form.cs
@@ -0,0 +1,94 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace GuessingGame
+{
+    public partial class Form : System.Windows.Forms.Form
+    {
+        int result;
+        Random random;
+        int @try;
+        float score;
+        float highscore;
+
+        public Form()
+        {
+            InitializeComponent();
+            InitializeVariables();
+        }
+
+        private void InitializeVariables()
+        {
+            random = new Random();
+            result = random.Next(0, 100);
+            //lbMessage.Text = string.Format("CHEAT: {0}", result); //ENABLE CHEAT
+            score = 0;
+            highscore = 0;
+            @try = 0;
+        }
+
+        private void btnGuess_Click(object sender, EventArgs e)
+        {
+            if (!int.TryParse(tb.Text, out int guess))
+            {
+                lbMessage.Text = "> Please enter a number to guess";
+            }
+            else
+            {
+                if (guess == result)
+                {
+                    Equal();
+                }
+                else if (guess < result)
+                {
+                    Less();
+                }
+                else
+                {
+                    Larger();
+                }
+            }
+
+        }
+
+        private void Equal()
+        {
+            lbMessage.Text = "> Congratulation!";
+            @try++;
+            score = 100f / @try;
+            if (score > highscore)
+            {
+                highscore = score;
+                lbHighscore.Text = string.Format("> Highscore - {0:0.00}%", highscore);
+            }
+        }
+
+        private void Less()
+        {
+            lbMessage.Text = "> Too low!";
+            @try++;
+        }
+
+        private void Larger()
+        {
+            lbMessage.Text = "> Too high!";
+            @try++;
+        }
+
+        private void btnReset_Click(object sender, EventArgs e)
+        {
+            lbMessage.Text = "> Please enter your guess";
+            result = random.Next(0, 100);
+            //lbMessage.Text = string.Format("CHEAT: {0}", result); //ENABLE CHEAT
+            @try = 0;
+            score = 0;
+        }
+    }
+}
